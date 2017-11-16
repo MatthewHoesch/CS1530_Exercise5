ee06fc2feb9a2b2842ddb41515e620a4c5222965
Matthew Hoesch <matthewhoesch@pitt.edu>
2017-11-15 23:25:59 -0500

    Add files via upload

--- Pi.java
+++ Pi.java
@@ -8,28 +8,31 @@ public class Pi {
 	public static AtomicLong threadCount = new AtomicLong(0);    
 	    
 	    public void monteCarloCalc(long num_threads, long num_iterations) {
+		//this is the amount of times the thread is allowed to do calculations
 	    final long multRatio = num_iterations / num_threads;
+		//thread array 
 		Thread[] threadsArray = new Thread[(int) num_threads];
-		
-		for (int j = 0; j < threadsArray.length; j++) {
-		    threadsArray[j] = new Thread(() -> {
-			    int currentCount = 0;
-			    int inside = 0;
-		while (currentCount++ < multRatio) {
-				double x = ThreadLocalRandom.current().nextDouble(1);
-				double y = ThreadLocalRandom.current().nextDouble(1);
+		for (int i = 0; i < threadsArray.length; i++) {
+		    threadsArray[i] = new Thread(() -> {
+			    int currentInteration = 0; //counting the number of times the thread is calculated 
+			    int inside = 0; //counting the number of inside random vals
+		while (currentInteration < multRatio) {
+				currentInteration++;	//increment the iteration number 
+				double x = ThreadLocalRandom.current().nextDouble(1);	//generate new random x
+				double y = ThreadLocalRandom.current().nextDouble(1);	//generate new random y
 				if ((x*x)+(y*y) < 1) 
-				    inside++;
+				    inside++;		//if random value sum is less than 1, increment inside random vals 
 			    }
 			    threadCount.addAndGet(inside);
 			});
 		}
-		
-		for (int j = 0; j < threadsArray.length; j++) 
-		    threadsArray[j].start();
-		for (int j = 0; j < threadsArray.length; j++) {
+		//start each thread inside the thread array
+		for (int i = 0; i < threadsArray.length; i++) 
+		    threadsArray[i].start();				
+		//stop each thread inside the thread array	
+		for (int i = 0; i < threadsArray.length; i++) {
 		    try {
-			threadsArray[j].join();
+			threadsArray[i].join();
 		    } catch (Exception ex) { }
 		}
 		
